ba7f85edf2c07000b81bb65270ab3e3ae1f04228
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-05-22 08:45:06 +0800

    pref(server): change static language id to import language id

--- packages/language-server/src/server/plugin.ts
+++ packages/language-server/src/server/plugin.ts
@@ -1,6 +1,10 @@
 import type { LanguagePlugin } from "@volar/language-core";
 import type { URI } from "vscode-uri";
-import { createWebgalVirtualCode, updateWebgalVirtualCode } from "./virtualCode";
+import {
+	createWebgalVirtualCode,
+	updateWebgalVirtualCode
+} from "./virtualCode";
+import { languageConfigId, languageId } from "@/utils/resources";
 
 const webgalLanguagePlugin: LanguagePlugin<URI> = {
 	getLanguageId(scriptId) {
@@ -10,35 +14,44 @@ const webgalLanguagePlugin: LanguagePlugin<URI> = {
 				path.endsWith("/game/config.txt") ||
 				path.endsWith("config.txt")
 			) {
-				return "webgal-config";
+				return languageConfigId;
 			}
 			if (path.endsWith(".txt")) {
-				return "webgal";
+				return languageId;
 			}
-			return "webgal";
+			return languageId;
 		}
 		if (path.endsWith("/game/config.txt")) {
-			return "webgal-config";
+			return languageConfigId;
 		}
 		if (path.endsWith(".txt") && path.includes("/game/scene/")) {
-			return "webgal";
+			return languageId;
 		}
 		return undefined;
 	},
-	createVirtualCode(scriptId, languageId, snapshot) {
-		if (languageId !== "webgal" && languageId !== "webgal-config") {
+	createVirtualCode(scriptId, langId, snapshot) {
+		if (langId !== languageId && langId !== languageConfigId) {
 			return;
 		}
 		return createWebgalVirtualCode(scriptId, languageId, snapshot);
 	},
 	updateVirtualCode(_scriptId, virtualCode, newSnapshot) {
 		if (
-			virtualCode.languageId !== "webgal" &&
-			virtualCode.languageId !== "webgal-config"
+			virtualCode.languageId !== languageId &&
+			virtualCode.languageId !== languageConfigId
 		) {
 			return;
 		}
 		return updateWebgalVirtualCode(virtualCode, newSnapshot);
+	},
+	isAssociatedFileOnly(scriptId, langId) {
+		if (langId === languageId) {
+			return scriptId.path.toLowerCase().endsWith(".txt");
+		}
+		if (langId === languageConfigId) {
+			return scriptId.path.toLowerCase().endsWith("/game/config.txt");
+		}
+		return false;
 	}
 };
 export default webgalLanguagePlugin;
